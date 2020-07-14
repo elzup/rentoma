92656cf512d0c8ef7144b1668b0c819c4934cc5f
elzup <guild0105@gmail.com>
2020-07-14 15:48:04 +0900

    update functions node10

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
+require('firebase-functions/lib/logger/compat')
+
 admin.initializeApp()
 
 const getTimes = () => {
@@ -57,7 +59,7 @@ function bodyValidation(contentType, body) {
 exports.log = functions.https.onRequest((req, response) => {
   const { headers, httpVersion, originalUrl, protocol, method } = req
   const body = bodyValidation(headers['content-type'], req.body)
-  filterHeadersKeys.forEach(key => {
+  filterHeadersKeys.forEach((key) => {
     delete headers[key]
   })
   const { timePath, timestamp } = getTimes()
@@ -77,24 +79,15 @@ exports.log = functions.https.onRequest((req, response) => {
     timestamp
   }
 
-  const callback = error => {
+  const callback = (error) => {
     if (error) {
       console.log('save error', error.message)
-      response
-        .status(500)
-        .send(error.message)
-        .end()
+      response.status(500).send(error.message).end()
     }
   }
 
-  admin
-    .database()
-    .ref('/log')
-    .push(log, callback)
-  admin
-    .database()
-    .ref(`/hour-log/${timePath}`)
-    .push(log, callback)
+  admin.database().ref('/log').push(log, callback)
+  admin.database().ref(`/hour-log/${timePath}`).push(log, callback)
 
   console.log('save success')
   response.status(200).end('log success')
